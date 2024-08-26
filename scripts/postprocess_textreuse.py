330234014405873a745d8ac82af8f72d5eb3315d
piconti <pauline.conti@epfl.ch>
2024-08-26 19:02:39 +0200

    update postprocessing script, needs large scale testing

--- scripts/postprocess_textreuse.py
+++ scripts/postprocess_textreuse.py
@@ -26,7 +26,7 @@ from dask.distributed import Client, LocalCluster
 # from dask_k8 import DaskCluster
 from impresso_commons.path.path_s3 import IMPRESSO_STORAGEOPT
 from datetime import datetime, timedelta
-import matplotlib
+from ast import literal_eval
 import dask.config
 from docopt import docopt
 from impresso_commons.utils.utils import init_logger
@@ -57,8 +57,13 @@ def unify_data(record: dict) -> dict:
             del record['title']
     else:
         title = ''
-    
+
     record['title'] = title
+
+    if 'id' in record and 'begin' in record and 'end' in record:
+        record['passages'] = "{}@{}:{}".format(record['id'], record['begin'], record['end'])
+    else:
+        record['passages'] = ''
 
     return record
 
@@ -74,8 +79,9 @@ def mint_document_ids(row) -> str:
     ])
     
 
-def lexical_overlap(row):
-    texts = row['text']
+def lexicaloverlap(texts):
+    #texts = row['text']
+    first = True
     intersection = list()
     
     longest_text_length = max([len(text) for text in texts])
@@ -97,6 +103,13 @@ def get_timedelta(min_date: str, max_date: str) -> timedelta:
     min = datetime.strptime(min_date, '%Y-%m-%d').date()
     max = datetime.strptime(max_date, '%Y-%m-%d').date()
     return max - min
+
+def eval_str_lists(cluster):
+    cluster['newspapers'] = literal_eval(cluster['newspapers'])
+    cluster['passages'] = literal_eval(cluster['passages'])
+    cluster['doc_ids'] = literal_eval(cluster['doc_ids'])
+    
+    return cluster
 
 
 def main() -> None:
@@ -140,13 +153,65 @@ def main() -> None:
     s3_output_path = f"{s3_path}/tr_clusters/"
 
     passim_data = db.read_text(
-        "s3_input_path/*.json", storage_options=IMPRESSO_STORAGEOPT
+        f"{s3_input_path}/*.json", storage_options=IMPRESSO_STORAGEOPT
     ).map(json.loads).persist() 
 
     passim_data_df = passim_data.map(unify_data).to_dataframe().persist()
     passim_data_df = client.gather(passim_data_df)
     passim_data_df = passim_data_df.set_index('uid').persist()
 
+      #clusters_df = passim_data_df.head(100000, compute=False, npartitions=10).sort_values('cluster')
+    clusters_df = passim_data_df.groupby('cluster').agg({'date': ['min', 'max'], 'size': 'count'}).compute()
+
+    # add the new columns also needing groupbys
+
+    # get the list of newspapers for which a cluster contains TR instances
+    np_sub = passim_data_df.groupby('cluster').apply(lambda r: sorted(r['series'].unique()), meta=('np', object)).compute()
+    clusters_df.loc[:, 'newspapers'] = np_sub
+
+    # get the list of passages for each cluster
+    passages_sub = passim_data_df.groupby('cluster')['passages'].apply(list, meta=('passages', object)).compute()
+    clusters_df.loc[:, 'passages'] = passages_sub
+
+    # compute the lexical overlap of passages within clusters
+    overlap_sub = passim_data_df.groupby('cluster')['text'].apply(lexicaloverlap, meta=('lo', object)).compute()
+    clusters_df.loc[:, 'lexical_overlap'] = overlap_sub
+
+    doc_ids_sub = passim_data_df.groupby('cluster')['id'].apply(lambda r: sorted(r.unique()), meta=('doc_ids', object)).compute()
+    clusters_df.loc[:, 'doc_ids'] = doc_ids_sub
+
+    # compute the time delta in days between the first and last occurence of TR within clusters.
+    clusters_df.loc[:,('date', 'min')] = pd.to_datetime(clusters_df.date['min'], format='%Y-%m-%d')
+    clusters_df.loc[:,('date', 'max')] = pd.to_datetime(clusters_df.date['max'], format='%Y-%m-%d')
+    clusters_df.loc[:,'time_delta'] = clusters_df.date['max'] - clusters_df.date['min']
+
+    clusters_df.loc[:, 'cluster_size'] = clusters_df['size']['count']
+    clusters_df.loc[:, 'min_date'] = clusters_df['date']['min'].astype('string')
+    clusters_df.loc[:, 'max_date'] = clusters_df['date']['max'].astype('string')
+    clusters_df['time_delta'] = clusters_df['time_delta'].apply(lambda x: x.days).astype('int64')
+    final_clusters_df =  clusters_df.drop([('date', 'min'),('date', 'max'), ('size', 'count')], axis=1)
+
+    final_clusters_df.loc[:, 'id'] = [f"tr-all-v1-24-c{x}" for x in final_clusters_df.index]
+
+    final_columns = ["id", "min_date", "max_date", "cluster_size", "time_delta", "newspapers", "passages", "doc_ids", "lexical_overlap"]
+    #final_clusters_df = final_clusters_df[final_columns]
+    #final_clusters_df = final_clusters_df.reset_index(drop = True)
+    final_clusters_df = final_clusters_df[final_columns].reset_index(drop = True)
+    final_clusters_df.columns = final_clusters_df.columns.droplevel(1)
+
+    clusters_ddf = dd.from_pandas(final_clusters_df, chunksize=100000)
+    clusters_bag = clusters_ddf.to_bag(format='dict').map(eval_str_lists).persist()
+
+    cluster_output_files = [
+        f"{s3_output_path}{str(n).zfill(4)}.jsonl.bz2"
+        for n in range(clusters_bag.npartitions)
+    ]
+
+    future = (
+        clusters_bag.map(json.dumps)
+        .repartition(len(cluster_output_files))
+        .to_textfiles(cluster_output_files, storage_options=IMPRESSO_STORAGEOPT)
+    )
 
 if __name__ == "__main__":
     main()
